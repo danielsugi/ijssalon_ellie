a573d65ad8755d727298af5c02ad93a9b040a2b3
daniel.sugiharto <daniel.sugiharto@printforce.nl>
2023-10-24 10:59:22 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,17 +1,27 @@
-prijzen = {
-  "aardbei" : 3,
-  "vanille" : 4,
-  "chocolade" : 5
-}
-aanbieding = prijzen ["aardbei"] * 0.8
+from helper import decoreer
 
-reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € "
-reclame_tekst2 = (f"{reclame_tekst}{aanbieding}") [:62]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split(" ")
-for el in reclame_tekst4 :
- if (len (el)) >= 5:
-  el= (el.upper())	
- else:
-  el= (el.lower())
- print (el)
+def print_aanbieding():
+  prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+  }
+  
+  aanbieding = prijzen ["aardbei"] * 0.8
+
+  reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+
+  reclame_tekst2 = reclame_tekst[:63]
+
+  reclame_tekst3 = reclame_tekst2.upper()
+
+  reclame_tekst4 = reclame_tekst3.split()
+
+  for el in reclame_tekst4 :
+    if len (el) > 4:
+      print(el.upper())	
+    else:
+      print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
